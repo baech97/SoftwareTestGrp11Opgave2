ad88cfc18c0a9deb2bf2662e6e14b7d203ba3aa3
Niels Stampe <niels.stampe@post.au.dk>
2019-03-12 13:37:57 +0100

    satte interfaces sammen med de rigtige klasser

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/PrintLog.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/PrintLog.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AirTrafficMonitorGrp11
 {
-    class PrintLog
+    class PrintLog : iPrintLog
     {
     }
 }
--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/TrafficDataSorter.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/TrafficDataSorter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AirTrafficMonitorGrp11
 {
-    class TrafficDataSorter
+    class TrafficDataSorter : iTrafficDataSorter
     {
     }
 }
