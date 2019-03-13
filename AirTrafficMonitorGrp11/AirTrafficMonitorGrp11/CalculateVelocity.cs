cf169fb03c90bedcd4f9a8def939f41fa6080906
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2019-03-13 12:19:48 +0100

    Ændret timestamp tilbage til datetime. Færdiggjort CalculateVelocity-event

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/CalculateVelocity.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/CalculateVelocity.cs
@@ -11,13 +11,13 @@ namespace AirTrafficMonitorGrp11
 
         private iTrafficDataSorter _dataSorter;
         public event EventHandler<List<TrackDataContainer>> VelocityCalculated;
+
         private int LastPosition_X;
         private int LastPosition_Y;
         private int CurrentPosition_X;
         private int CurrentPosition_Y;
         private DateTime LastTime = DateTime.Now;
         private DateTime CurrentTime = DateTime.Now;
-
         private double timediff;
         private double Velocity;
 
@@ -41,18 +41,19 @@ namespace AirTrafficMonitorGrp11
                     TimeSpan ts = CurrentTime - LastTime;
                     timediff = ts.Milliseconds;
 
-                    Velocity = timediff * (((CurrentPosition_X - LastPosition_X) ^ 2) + ((CurrentPosition_Y - LastPosition_Y) ^ 2));
+                    Velocity = timediff * (((CurrentPosition_X - LastPosition_X) ^ 2) +
+                                           ((CurrentPosition_Y - LastPosition_Y) ^ 2));
+                    data.Velocity = Velocity;
+                    tdcList.Add(data);
                 }
 
                 LastPosition_X = data.X;
                 LastPosition_Y = data.Y;
                 LastTime = data.Timestamp;
-
-                data.Velocity = Velocity;
-                tdcList.Add(data);
-
-                ;
             }
+
+            VelocityCalculated?.Invoke(this, tdcList);
+
         }
     }
 }
