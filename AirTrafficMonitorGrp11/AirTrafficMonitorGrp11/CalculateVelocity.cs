f30349a62c1cfb94bb0bab864396f4f3cb2e0f9d
Niels Stampe <niels.stampe@post.au.dk>
2019-03-13 11:19:07 +0100

    velocity alg

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/CalculateVelocity.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/CalculateVelocity.cs
@@ -12,19 +12,21 @@ namespace AirTrafficMonitorGrp11
         private int LastPosition_Y;
         private int CurrentPosition_X;
         private int CurrentPosition_Y;
-        private DateTime LastTime = DateTime.UtcNow;
+        private DateTime LastTime = DateTime.Now;
         private DateTime CurrentTime = DateTime.Now;
-        private double timediff;
 
+        private double timediff;
         private double Velocity;
 
 
         public void CalculateVelocity()
         {
-            Velocity = (CurrentTime - LastTime) *
-                       ((CurrentPosition_X - LastPosition_X) ^ 2 + (CurrentPosition_Y - LastPosition_Y) ^ 2);
+            TimeSpan ts = CurrentTime - LastTime;
+            timediff = ts.Milliseconds;
 
-            timediff = CurrentTime - LastTime;
+            Velocity = timediff * (((CurrentPosition_X - LastPosition_X) ^ 2) + ((CurrentPosition_Y - LastPosition_Y) ^ 2));
+
+
 
         }
 
