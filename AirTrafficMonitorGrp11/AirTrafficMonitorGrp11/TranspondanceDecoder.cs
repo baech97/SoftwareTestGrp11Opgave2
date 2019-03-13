f58c76ecdf59bed56166655be0ddca8e6161fd09
MikkelRBoisen <mikkel.rugholm.boisen@post.au.dk>
2019-03-13 11:16:31 +0100

    tilføjet split af string

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/TranspondanceDecoder.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/TranspondanceDecoder.cs
@@ -10,6 +10,8 @@ namespace AirTrafficMonitorGrp11
     class TranspondanceDecoder : iTranspondanceDecoder
     {
         private ITransponderReceiver _transponderReceiver;
+        TrackDataContainer _tdc;
+        
 
         public TranspondanceDecoder(ITransponderReceiver transponderReceiver)
         {
@@ -19,9 +21,16 @@ namespace AirTrafficMonitorGrp11
 
         public void OnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
         {
+            string[] inputFields;
             foreach (var data in e.TransponderData)
             {
-                
+                _tdc = new TrackDataContainer();
+                inputFields = data.Split(';');
+                _tdc.Tag = Convert.ToString(inputFields[0]);
+                _tdc.X = Convert.ToInt32(inputFields[1]);
+                _tdc.Y = Convert.ToInt32(inputFields[2]);
+                _tdc.Altitude = Convert.ToInt32(inputFields[3]);
+                _tdc.Timestamp = Convert.ToDateTime(inputFields[4]);
             }
         }
 
