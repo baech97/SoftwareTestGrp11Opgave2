70b6c057bb588e5e312207fe33efb3b3fb35aba8
Niels Stampe <niels.stampe@post.au.dk>
2019-03-19 12:42:34 +0100

    Test af calculate course

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateCourseTest.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateCourseTest.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace AirTrafficMonitorGrp11.Unit.Test
 {
+    [TestFixture]
     class CalculateCourseTest
     {
+        private CalculateCourse _uut;
+        private ICalculateCourse _calculateCourse;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculateCourse = NSubstitute.Substitute.For<ICalculateCourse>()
+        }
+
+
+        [Test]
+        public void Course_is_90()
+        {
+
+        }
+
     }
 }
