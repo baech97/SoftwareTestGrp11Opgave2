490e6782d036defa545915db87be78c52ef8d23e
Niels Stampe <niels.stampe@post.au.dk>
2019-03-19 14:15:38 +0100

    ændring i test af course

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateCourseTest.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateCourseTest.cs
@@ -10,22 +10,28 @@ namespace AirTrafficMonitorGrp11.Unit.Test
     [TestFixture]
     class CalculateCourseTest
     {
-        private CalculateVelocity _uut;
+        private CalculateCourse _uut;
         private iCalculateCourse _calculateCourse;
-        private iTrafficDataSorter _trafficDataSorter;
+        private iCalculateVelocity _velocity;
 
         [SetUp]
         public void SetUp()
         {
             _calculateCourse = NSubstitute.Substitute.For<iCalculateCourse>();
-            _uut = new CalculateVelocity(_trafficDataSorter);
+            _uut = new CalculateCourse(_trafficDataSorter);
+
+
         }
 
 
         [Test]
         public void Course_is_90()
         {
-            List<string> testData = new List<string>();
+            TrackDataContainer tdc = new TrackDataContainer();
+            tdc.X = 2000;
+            tdc.Y = 4000;
+            
+            Assert.That(_uut.OnVelocityCalculated(this,tdc) Is.EqualTo(45));
             
         }
 
