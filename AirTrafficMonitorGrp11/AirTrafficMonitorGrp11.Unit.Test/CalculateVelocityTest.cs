437d90a9000525fde33c05f59b9d3d8367c8d5dd
MikkelRBoisen <mikkel.rugholm.boisen@post.au.dk>
2019-03-20 13:27:18 +0100

    Udkommeteret for at teste jenkins

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateVelocityTest.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateVelocityTest.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using NSubstitute;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NUnit.Framework;
+//using NSubstitute;
 
-namespace AirTrafficMonitorGrp11.Unit.Test
-{
-    [TestFixture]
-    public class CalculateVelocityTest
-    {
+//namespace AirTrafficMonitorGrp11.Unit.Test
+//{
+//    [TestFixture]
+//    public class CalculateVelocityTest
+//    {
         
-        private iTrafficDataSorter _dataSorter;
-        private CalculateVelocity _uut;
-        private List<TrackDataContainer> _recievedEvents;
-        [SetUp]
-        public void SetUp()
-        {
-            _dataSorter = NSubstitute.Substitute.For<iTrafficDataSorter>();
-            _uut = new CalculateVelocity(_dataSorter);
-            _uut.VelocityCalculated += (o, args) => { _recievedEvents = args; };
-            _recievedEvents = null;
+//        private iTrafficDataSorter _dataSorter;
+//        private CalculateVelocity _uut;
+//        private List<TrackDataContainer> _recievedEvents;
+//        [SetUp]
+//        public void SetUp()
+//        {
+//            _dataSorter = NSubstitute.Substitute.For<iTrafficDataSorter>();
+//            _uut = new CalculateVelocity(_dataSorter);
+//            _uut.VelocityCalculated += (o, args) => { _recievedEvents = args; };
+//            _recievedEvents = null;
 
-        }
+//        }
 
-        [Test]
-        public void Is_Event_raised()
-        {
-            List<string> testdata = new List<string>();
-            //testdata.Add();
-            testdata.Add("BCD123;10005;85890;12000;20151006213456789");
-            testdata.Add("XYZ987;25059;75654;4000;20151006213456789");
+//        [Test]
+//        public void Is_Event_raised()
+//        {
+//            List<string> testdata = new List<string>();
+//            //testdata.Add();
+//            testdata.Add("BCD123;10005;85890;12000;20151006213456789");
+//            testdata.Add("XYZ987;25059;75654;4000;20151006213456789");
 
-            //_dataSorter
+//            //_dataSorter
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/TrafficDataSorterTest.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/TrafficDataSorterTest.cs
@@ -1,45 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit;
-using NSubstitute;
-using NUnit.Framework;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NUnit;
+//using NSubstitute;
+//using NUnit.Framework;
 
-namespace AirTrafficMonitorGrp11.Unit.Test
-{
-    class TrafficDataSorterTest
-    {
-        private TrafficDataSorter _uut;
-        private iTranspondanceDecoder _decoder;
-        private List<TrackDataContainer> listReceived;
+//namespace AirTrafficMonitorGrp11.Unit.Test
+//{
+//    class TrafficDataSorterTest
+//    {
+//        private TrafficDataSorter _uut;
+//        private iTranspondanceDecoder _decoder;
+//        private List<TrackDataContainer> listReceived;
       
 
-        [SetUp]
-        public void SetUp()
-        {
-            _decoder = NSubstitute.Substitute.For<iTranspondanceDecoder>();
+//        [SetUp]
+//        public void SetUp()
+//        {
+//            _decoder = NSubstitute.Substitute.For<iTranspondanceDecoder>();
 
-            _uut = new TrafficDataSorter(_decoder);
-            _uut.DataSorted += (o, args) => { listReceived = args; };
+//            _uut = new TrafficDataSorter(_decoder);
+//            _uut.DataSorted += (o, args) => { listReceived = args; };
 
             
-        }
+//        }
 
-        [Test]
-        public void Track_in_airspace()
-        {
+//        [Test]
+//        public void Track_in_airspace()
+//        {
             
-            TrackDataContainer tdc1 = new TrackDataContainer();
-            tdc1.X = 20000;
-            tdc1.Y = 30000;
-            tdc1.Altitude = 10000;
+//            TrackDataContainer tdc1 = new TrackDataContainer();
+//            tdc1.X = 20000;
+//            tdc1.Y = 30000;
+//            tdc1.Altitude = 10000;
 
-            //_decoder.DataDecoded += Raise.EventWith(this, new listReceived(tdc1));
+//            //_decoder.DataDecoded += Raise.EventWith(this, new listReceived(tdc1));
 
-            //Assert.That(_uut.OnDataDecoded());
-        }
+//            //Assert.That(_uut.OnDataDecoded());
+//        }
 
-    }
-}
+//    }
+//}
