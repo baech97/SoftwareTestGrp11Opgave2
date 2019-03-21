fb13b9cb7de5afe1c0eaa81395b6cf35bf1a8a02
Niels Stampe <niels.stampe@post.au.dk>
2019-03-21 14:00:05 +0100

    data sorter unit test done

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/TrafficDataSorterTest.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/TrafficDataSorterTest.cs
@@ -41,5 +41,54 @@ namespace AirTrafficMonitorGrp11.Unit.Test
 
             Assert.That(_uut.DataRecivedList, Is.EqualTo(dataList));
         }
+
+
+        [TestCase(10000, 20000, 10000)]
+        [TestCase(20000, 10000, 10000)]
+        [TestCase(20000, 30000, 500)]
+        [TestCase(90000, 30000, 10000)]
+        [TestCase(20000, 90000, 10000)]
+        [TestCase(20000, 30000, 20000)]
+        public void Track_inside_airspace(int s1, int s2, int s3) 
+        {
+            List<TrackDataContainer> dataList = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = s1;
+            container.Y = s2;
+            container.Altitude = s3;
+            container.Timestamp = DateTime.Now;
+
+            dataList.Add(container);
+
+            Assert.That(_uut.SortData(dataList), Is.EqualTo(dataList));
+        }
+
+
+
+        [TestCase(9999, 20000, 10000)]
+        [TestCase(20000, 9999, 10000)]
+        [TestCase(20000,30000, 499)]
+        [TestCase(90001, 30000, 10000)]
+        [TestCase(20000, 90001, 10000)]
+        [TestCase(20000, 30000, 20001)]
+        public void Track_outside_airspace(int s1, int s2, int s3)
+        {
+            List<TrackDataContainer> dataList = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = s1;
+            container.Y = s2;
+            container.Altitude = s3;
+            container.Timestamp = DateTime.Now;
+
+            dataList.Add(container);
+
+            Assert.That(_uut.SortData(dataList), Is.Not.EqualTo(dataList));
+        }
+
+
     }
 }
--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/TrafficDataSorter.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11/TrafficDataSorter.cs
@@ -22,29 +22,35 @@ namespace AirTrafficMonitorGrp11
 
         public void OnDataDecoded(object sender, ATMEvent e)
         {
-                DataRecivedList = e._tdcList;
-                
-                List<TrackDataContainer> tdcList = new List<TrackDataContainer>();
-                foreach (var data in e._tdcList)
-                {
-                    if (data.X > 10000 && data.X < 90000)
-                    {
-                        if (data.Y > 10000 && data.Y < 90000)
-                        {
-                            if (data.Altitude > 500 && data.Altitude < 20000)
-                            {
-                                tdcList.Add(data);
-                            }
-                        }
-                    }
-                }
+            DataRecivedList = e._tdcList;
+            List<TrackDataContainer> tdcList = new List<TrackDataContainer>();
+            tdcList = SortData(DataRecivedList);
 
             if (tdcList.Count != 0)
             {
                 ATMEvent atmEvent = new ATMEvent(tdcList);
                 DataSorted?.Invoke(this, atmEvent);
             }
-                     
+
+        }
+
+        public List<TrackDataContainer> SortData(List<TrackDataContainer> trackList)
+        {
+            List<TrackDataContainer> list = new List<TrackDataContainer>();
+            foreach (var data in trackList)
+            {
+                if (data.X >= 10000 && data.X <= 90000)
+                {
+                    if (data.Y >= 10000 && data.Y <= 90000)
+                    {
+                        if (data.Altitude >= 500 && data.Altitude <= 20000)
+                        {
+                            list.Add(data);
+                        }
+                    }
+                }
+            }
+            return list;
         }
     }
 }
