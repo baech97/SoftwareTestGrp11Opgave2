cf59d4c45ec70e04775d028dbc18bb91dbb29468
Niels Stampe <niels.stampe@post.au.dk>
2019-03-21 12:51:06 +0100

    Alle unit test til course calc der har med udregninger at gøre er nu lavet

--- AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateCourseTest.cs
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/CalculateCourseTest.cs
@@ -22,7 +22,7 @@ namespace AirTrafficMonitorGrp11.Unit.Test
         }
 
         [Test]
-        public void Course_is_45()
+        public void NE_Course_is_45()
         {
             List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
             List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
@@ -45,45 +45,219 @@ namespace AirTrafficMonitorGrp11.Unit.Test
             LastFlightData.Add(container2);
 
             _uut.LastFlightData = LastFlightData;
-            _uut.Calculate(CurrentFlights);
-
 
             _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
 
             Assert.That(_uut.Course, Is.EqualTo(45));
         }
 
+        [Test]
+        public void SE_Course_is_135()
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
 
-        //private CalculateCourse _uut;
-        //private iCalculateCourse _calculateCourse;
-        //private iCalculateVelocity _velocity;
-        //private List<TrackDataContainer> listReceived;
-        //private List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 20000;
+            container.Y = 10000;
+            container.Altitude = 10000;
+            container.Timestamp = DateTime.Now;
+
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 10000;
+            container2.Y = 20000;
+            container2.Altitude = 10000;
+            container2.Timestamp = DateTime.Now;
+
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
+
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(135));
+        }
+
+        [Test]
+        public void SW_Course_is_225()
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 10000;
+            container.Y = 10000;
+            container.Altitude = 10000;
+            container.Timestamp = DateTime.Now;
+
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 20000;
+            container2.Y = 20000;
+            container2.Altitude = 10000;
+            container2.Timestamp = DateTime.Now;
+
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
+
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(225));
+        }
+
+        [Test]
+        public void NW_Course_is_315()
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 10000;
+            container.Y = 20000;
+            container.Altitude = 10000;
+            container.Timestamp = DateTime.Now;
+
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 20000;
+            container2.Y = 10000;
+            container2.Altitude = 10000;
+            container2.Timestamp = DateTime.Now;
+
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
+
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(315));
+        }
+
+        [Test]
+        public void North_Course_is_0()
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 10000;
+            container.Y = 20000;
+            container.Altitude = 10000;
+            container.Timestamp = DateTime.Now;
+
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 10000;
+            container2.Y = 10000;
+            container2.Altitude = 10000;
+            container2.Timestamp = DateTime.Now;
+
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
+
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void South_Course_is_180()
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 10000;
+            container.Y = 10000;
+            container.Altitude = 10000;
+            container.Timestamp = DateTime.Now;
+
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 10000;
+            container2.Y = 20000;
+            container2.Altitude = 10000;
+            container2.Timestamp = DateTime.Now;
+
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
+
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(180));
+        }
+
+        [Test]
+        public void East_Course_is_90()
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 20000;
+            container.Y = 10000;
+            container.Altitude = 10000;
+            container.Timestamp = DateTime.Now;
+
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 10000;
+            container2.Y = 10000;
+            container2.Altitude = 10000;
+            container2.Timestamp = DateTime.Now;
+
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
+
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(90));
+        }
+
+        [Test]
+        public void West_Course_is_270() 
+        {
+            List<TrackDataContainer> CurrentFlights = new List<TrackDataContainer>();
+            List<TrackDataContainer> LastFlightData = new List<TrackDataContainer>();
+
+            TrackDataContainer container = new TrackDataContainer();
+            container.Tag = "ATR423";
+            container.X = 10000;
+            container.Y = 10000;
 
 
-
-        //private int LastPosition_X;
-        //private int LastPosition_Y;
-        //private int CurrentPosition_X;
-        //private int CurrentPosition_Y;
-
-
-        //[SetUp]
-        //public void SetUp()
-        //{
-        //    _calculateCourse = NSubstitute.Substitute.For<iCalculateCourse>();
-
-        //    _uut = new CalculateCourse(_velocity);
-        //    _uut.CourseCalculated += (o, args) => { listReceived = args; };
-        //}
+            TrackDataContainer container2 = new TrackDataContainer();
+            container2.Tag = "ATR423";
+            container2.X = 20000;
+            container2.Y = 10000;
 
 
-        //[Test]
-        //public void Course_is_120()
-        //{
-        //    _uut.;
+            CurrentFlights.Add(container);
+            LastFlightData.Add(container2);
 
-        //}
+            _uut.LastFlightData = LastFlightData;
+
+            _receiver.VelocityCalculated += Raise.EventWith(this, new ATMEvent(CurrentFlights));
+
+            Assert.That(_uut.Course, Is.EqualTo(270));
+        }
 
     }
 }
