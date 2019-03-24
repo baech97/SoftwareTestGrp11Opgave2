fa955474cf38486ba8e4782e76e615369a876643
MikkelRBoisen <mikkel.rugholm.boisen@post.au.dk>
2019-03-24 14:14:08 +0100

    Oprettet SeperationCheckerTest

---
+++ AirTrafficMonitorGrp11/AirTrafficMonitorGrp11.Unit.Test/SeperationCheckerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using TransponderReceiver;
+
+namespace AirTrafficMonitorGrp11.Unit.Test
+{
+    [TestFixture]
+    public class SeperationCheckerTest
+    {
+        private iTrafficDataSorter _dataSorter;
+        private SeperationChecker _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dataSorter = NSubstitute.Substitute.For<iTrafficDataSorter>();
+            _uut = new SeperationChecker(_dataSorter);
+        }
+
+        
+
+    }
+}
